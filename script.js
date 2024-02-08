1b958f9b09a32debe9aa1b18ac4ebc9e1173fdb0
Luka Goginashvili <145713150+blodwenlixiss@users.noreply.github.com>
2024-02-08 16:05:05 +0400

    Promise added

--- script.js
+++ script.js
@@ -20,123 +20,166 @@
 //  თუ რომელიმე ცარიელი იქნება ფორმა არ უნდა დასაბმითდეს;
 // ფორმის დასაბმითებისას დახურეთ მოდალი;
 // closeზე დაჭერიას უნდა დახუროთ მოდალი.
-const tableBody = document.getElementById("tableBody");
-const addModalPg = document.getElementById("modalAdd");
-const addUser = document.getElementById("add");
-const modal = document.querySelector(".modal");
-const overlay = document.querySelector(".overlay");
 
-const form = document.getElementById("modalForm");
-const closeBtn = document.getElementById("close");
+// const tableBody = document.getElementById("tableBody");
+// const addModalPg = document.getElementById("modalAdd");
+// const addUser = document.getElementById("add");
+// const modal = document.querySelector(".modal");
+// const overlay = document.querySelector(".overlay");
 
-const users = [
-  {
-    id: 7,
-    email: "michael.lawson@reqres.in",
-    first_name: "Michael",
-    last_name: "Lawson",
-  },
-  {
-    id: 8,
-    email: "lindsay.ferguson@reqres.in",
-    first_name: "Lindsay",
-    last_name: "Ferguson",
-  },
-  {
-    id: 9,
-    email: "tobias.funke@reqres.in",
-    first_name: "Tobias",
-    last_name: "Funke",
-  },
-  {
-    id: 10,
-    email: "byron.fields@reqres.in",
-    first_name: "Byron",
-    last_name: "Fields",
-  },
-  {
-    id: 11,
-    email: "george.edwards@reqres.in",
-    first_name: "George",
-    last_name: "Edwards",
-  },
-  {
-    id: 12,
-    email: "rachel.howell@reqres.in",
-    first_name: "Rachel",
-    last_name: "Howell",
-  },
-];
+// const form = document.getElementById("modalForm");
+// const closeBtn = document.getElementById("close");
 
-users.forEach((e) => {
-  const deleteBtn = document.createElement("button");
-  const tableRow = document.createElement("tr");
-  deleteBtn.textContent = "delete";
-  tableBody.appendChild(tableRow);
-  tableRow.innerHTML += `<td>${e.first_name}</td>
-                           <td>${e.last_name}</td>
-                           <td>${e.email}</td>
-                           `;
-  tableRow.append(deleteBtn);
-  deleteBtn.addEventListener("click", () => {
-    tableRow.remove();
-  });
-});
+// const users = [
+//   {
+//     id: 7,
+//     email: "michael.lawson@reqres.in",
+//     first_name: "Michael",
+//     last_name: "Lawson",
+//   },
+//   {
+//     id: 8,
+//     email: "lindsay.ferguson@reqres.in",
+//     first_name: "Lindsay",
+//     last_name: "Ferguson",
+//   },
+//   {
+//     id: 9,
+//     email: "tobias.funke@reqres.in",
+//     first_name: "Tobias",
+//     last_name: "Funke",
+//   },
+//   {
+//     id: 10,
+//     email: "byron.fields@reqres.in",
+//     first_name: "Byron",
+//     last_name: "Fields",
+//   },
+//   {
+//     id: 11,
+//     email: "george.edwards@reqres.in",
+//     first_name: "George",
+//     last_name: "Edwards",
+//   },
+//   {
+//     id: 12,
+//     email: "rachel.howell@reqres.in",
+//     first_name: "Rachel",
+//     last_name: "Howell",
+//   },
+// ];
 
-const Success = function (text) {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-  users.push(text);
-};
+// users.forEach((e) => {
+//   const deleteBtn = document.createElement("button");
+//   const tableRow = document.createElement("tr");
+//   deleteBtn.textContent = "delete";
+//   tableBody.appendChild(tableRow);
+//   tableRow.innerHTML += `<td>${e.first_name}</td>
+//                            <td>${e.last_name}</td>
+//                            <td>${e.email}</td>
+//                            `;
+//   tableRow.append(deleteBtn);
+//   deleteBtn.addEventListener("click", () => {
+//     tableRow.remove();
+//   });
+// });
 
-const text = document.getElementById("text");
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  const validateNames = (names) => {
-    return String(names).match(/^[a-z ,.'-]+$/i);
-  };
+// const Success = function (text) {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+//   users.push(text);
+// };
 
-  const formValues = document.forms.modalForm;
-  try {
-    if (formValues.id.value === "" || formValues.id.value.length !== 11) {
-      throw new Error("Invalid ID");
-    }
-    const validateEmail = (email) => {
-      return String(email)
-        .toLowerCase()
-        .match(
-          /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
-        );
-    };
+// const text = document.getElementById("text");
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   const validateNames = (names) => {
+//     return String(names).match(/^[a-z ,.'-]+$/i);
+//   };
 
-    if (!validateEmail(formValues.email.value)) {
-      throw new Error("Invalid Email");
-    }
-    if (!validateNames(formValues.firstName.value)) {
-      throw new Error("Invalid Name");
-    }
-    if (!validateNames(formValues.lastName.value)) {
-      throw new Error("Invalid Last Name");
-    }
-    const data = {
-      id: formValues.id.value,
-      email: formValues.email.value,
-      first_name: formValues.firstName.value,
-      last_name: formValues.lastName.value,
-    };
-    Success(data);
-    form.reset();
-  } catch (err) {
-    text.textContent = err.message;
-    text.style.color = "red";
-  }
-});
+//   const formValues = document.forms.modalForm;
+//   try {
+//     if (formValues.id.value === "" || formValues.id.value.length !== 11) {
+//       throw new Error("Invalid ID");
+//     }
+//     const validateEmail = (email) => {
+//       return String(email)
+//         .toLowerCase()
+//         .match(
+//           /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+//         );
+//     };
 
-addModalPg.addEventListener("click", () => {
-  modal.classList.remove("hidden");
-  overlay.classList.remove("hidden");
-});
-closeBtn.addEventListener("click", () => {
-  modal.classList.add("hidden");
-  overlay.classList.add("hidden");
-});
+//     if (!validateEmail(formValues.email.value)) {
+//       throw new Error("Invalid Email");
+//     }
+//     if (!validateNames(formValues.firstName.value)) {
+//       throw new Error("Invalid Name");
+//     }
+//     if (!validateNames(formValues.lastName.value)) {
+//       throw new Error("Invalid Last Name");
+//     }
+//     const data = {
+//       id: formValues.id.value,
+//       email: formValues.email.value,
+//       first_name: formValues.firstName.value,
+//       last_name: formValues.lastName.value,
+//     };
+//     Success(data);
+//     form.reset();
+//   } catch (err) {
+//     text.textContent = err.message;
+//     text.style.color = "red";
+//   }
+// });
+
+// addModalPg.addEventListener("click", () => {
+//   modal.classList.remove("hidden");
+//   overlay.classList.remove("hidden");
+// });
+// closeBtn.addEventListener("click", () => {
+//   modal.classList.add("hidden");
+//   overlay.classList.add("hidden");
+// });
+
+// დავალება 2
+// დაწერეთ  ასინქრონული ფუნქცია, რომელიც არგუმენტად მიიღებს სტრინგს;
+// თუ ამ სტრინგის სიგრძე მეტია 10ზე ფრომისის resolve ბლოკში  გამოიტანეთ თვითონ  სტრინგი;
+// სხვა შემთხვევაში  ფრომისის reject ბლოკში გამოიტანეთ ტექსტი " This is async function";
+// ორივე შემთხვევაში, ტექსტი უნდა გამოიტანოთ  setTimeout მეთოდში 5 წამის დაყოვნებით.
+
+// გამოიყენეთ Promise მეთოდი, ისე როგორც ლექციაზე გავაკეთეთ.
+
+// const myPromise = function (str) {
+//   return new Promise((resolve, reject) => {
+//     if (str.length < 10) {
+//       setTimeout(() => {
+//         resolve("string is less than 10");
+//       }, 5000);
+//     } else {
+//       setTimeout(() => {
+//         reject("string is more than 10");
+//       }, 5000);
+//     }
+//   });
+// };
+// const myNewPromise = function (str) {
+//   return new Promise((resolve, reject) => {
+//     if (str.length > 10) {
+//       setTimeout(() => {
+//         resolve("string is more than 10");
+//       }, 5000);
+//     } else {
+//       setTimeout(() => {
+//         reject("string is less than 10");
+//       }, 5000);
+//     }
+//   });
+// };
+// Promise.all([myPromise("luka"), myNewPromise("vighacisdidisaxeli")])
+//   .then((response) => {
+//     console.log(response);
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
